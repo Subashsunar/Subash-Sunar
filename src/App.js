483fd53d5e0c0a93f1cffc83e67ab15708013184
Rudra Bishwokarma <rudrabishwokarma@Rudras-MacBook-Pro.local>
2025-05-21 23:10:43 -0700

    Making a minor changes in exiting pages and added Landing page (Main.js).

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 /*
  File: App.js
  Project: React Application
- Created on: 2023-10-05
- Last Modified: 2023-10-05
+ Created on: 05-21-2025
+ Last Modified: 05-21-2025
  Author: Subash Sunar
- Date: 2023-10-05
+ Date: 05-21-2025
  Description: This is a main file of the React application. It imports the necessary modules and components.
  */
 
 
 import './App.css';
+import Main from './Main';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>  
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="main-content">
+                <h1>Welcome to My Portfolio</h1>
+                <p>Hi, I'm Subash Sunar, a passionate web developer.</p>
+                <a href="#projects" className="btn">View Projects</a>
+            </div>
+        </div>
+    );
+}
+export default Main;
